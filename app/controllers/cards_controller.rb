9a7373016813691ece1c34150f7cc27d0791512e
José Emiliano Cabrera Blancas <jemiliano.cabrera@gmail.com>
2016-08-11 15:46:06 -0500

    Payment controller
    It was added the payment call in the CardController
    This will create a payment, who is going to be process
    by some job.

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,26 +1,80 @@
+# coding: utf-8
 class CardsController < ApplicationController
 
   def create
-    card  = Card.new(card_params)
-    token = card.save
+    @card  = Card.new(card_params)
+    token = @card.save
 
     if token
-      card_response = card.to_permitted_json
-      card_response[:token] = token
-      render json: {
-               success: true,
-               card: card_response
-             }
+      @card = @card.to_permitted_json
+      @card[:token] = token
+
+      render_success_tokenization
     else
-      render json: {
-               success: false,
-               errors: card.errors
-             }, status: 400
+      render_fail_tokenization
     end
+  end
+
+  def charge
+    card = Card.detokenize(params[:token])
+    @payment = Payment.new(payment_params)
+
+    if card
+      if @payment.save
+        Card.destroy(params[:token])
+        render_payment_success
+      else
+        render_payment_errors
+      end
+    else
+      render_invalid_token
+    end
+
   end
 
   private
   def card_params
     params.require(:card).permit(:name, :number, :cvc)
+  end
+
+  def payment_params
+    params.require(:payment).permit(:amount)
+  end
+
+  def render_success_tokenization
+    render json: {
+             success: true,
+             card: @card
+           }
+
+  end
+
+  def render_fail_tokenization
+    render json: {
+             success: false,
+             errors: @card.errors
+           }, status: 400
+
+  end
+
+  def render_payment_success
+    render json: {
+             success: true,
+             payment: @payment.as_json
+           }
+  end
+
+  def render_payment_errors
+    render json: {
+             success: false,
+             errors: @payment.errors.messages
+           }, status: 400
+  end
+
+  def render_invalid_token
+    render json: {
+             success: false,
+             errors: { token:"token inválido" }
+           }, status: 404
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
   post '/cards/tokens/', to: 'cards#create'
+  put  '/cards/charge/', to: 'cards#charge'
   
 end
