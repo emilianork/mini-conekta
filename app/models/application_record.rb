b2a13dbdc7b40b7e9510ee15dd33eef440d95d31
José Emiliano Cabrera Blancas <jemiliano.cabrera@gmail.com>
2016-08-11 12:17:39 -0500

    ApplicationRecord
    ApplicationRecord not longer inherets from ActiveRecord. I also
    create a validations method that search all model validators from
    'app/models/validators/'. It is important that every class there
    have to finish its name with validator.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,13 @@
-class ApplicationRecord < ActiveRecord::Base
-  self.abstract_class = true
+class ApplicationRecord
+  include Redis::Objects
+
+  def validates(attribute, validators={})
+    validators.each do |key, value|
+      klass_name = "validators/#{key.to_s}_validator".classify
+      klass = klass_name.constantize
+
+      klass.new(model: self, attribute: attribute).validate(value)
+    end
+  end
+
 end
